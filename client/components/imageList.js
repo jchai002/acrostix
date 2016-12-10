8bcd5fe1b17fcf8d530fc9ed0500faa53a249a26
Jerry Chai <jerry@gooddogdesign.com>
2016-12-09 17:43:56 -0800

    add new imageList component

---
+++ client/components/imageList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const ImageList = () => {
+  render() {
+    return (
+      <ul>
+        <li>Image Detail</li>
+      </ul>
+    );
+  }
+}
+
+export default ImageList
