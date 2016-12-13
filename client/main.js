fdda53cba3514f4153c4c87f1d720e6ac1184109
Jerry Chai <jerry@gooddogdesign.com>
2016-12-12 20:22:43 -0800

    add store to puzzle container

--- client/main.js
+++ client/main.js
@@ -1,14 +1,17 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import Puzzle from './components/puzzle';
-const App = () => {
-  return (
-    <div>
-      <Puzzle />
-    </div>
-  );
-};
+
+store = configureStore();
 
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.react-top-level'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <Puzzle />
+    </Provider>,
+    document.querySelector('.react-top-level');
+  );
 });
