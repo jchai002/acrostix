40b7160b84c6d42cdba65970d629fd738e6f279b
Jerry Chai <jerry@gooddogdesign.com>
2016-12-14 14:11:22 -0800

    wrap actions in thunk

--- client/actions/letterActions.js
+++ client/actions/letterActions.js
@@ -1,5 +1,24 @@
 import * as types from './actionTypes.js'
 
+// thunk
 export function addLetter(letter) {
-  return {type: types.ADD_LETTER, letter}
+  return function(dispatch) {
+    // simply dispatch success event for now. Will check for db call success in futures
+    // if api call succeeds, call addLetterSuccess with .then, else use .catch for errors
+    dispatch(addLetterSuccess(letter));
+  }
+}
+
+export function addLetterSuccess(letter) {
+  return {type: types.ADD_LETTER_SUCCESS, letter}
+}
+
+export function updateLetter(letter) {
+  return function(dispatch) {
+    dispatch(updateLetterSuccess(letter));
+  }
+}
+
+export function updateLetterSuccess(letter) {
+  return {type: types.UPDATE_LETTER_SUCCESS, letter}
 }
--- client/reducers/letterReducer.js
+++ client/reducers/letterReducer.js
@@ -2,7 +2,7 @@ import * as types from '../actions/actionTypes.js'
 
 export default function letterReducer(state =[],action) {
   switch (action.type) {
-    case types.ADD_LETTER:
+    case types.ADD_LETTER_SUCCESS:
       return [...state,
         Object.assign({}, action.letter)
       ]
