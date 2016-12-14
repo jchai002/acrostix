b2975b6f541b762f39053f2aead2e3cd35247a26
Jerry Chai <jerry@gooddogdesign.com>
2016-12-14 15:43:28 -0800

    add business logic pseudocode

--- client/reducers/letterReducer.js
+++ client/reducers/letterReducer.js
@@ -10,6 +10,10 @@ export default function letterReducer(state = initialState.letters,action) {
     case types.UPDATE_LETTER_WORDID_SUCCESS:
       console.log('update letter word id')
         // need to find the first letter in state that matches word id of the payload and return new state
+        // iterate through current state, use JSON.stringify to find the first letter object that matches the char
+        // replace the value of WordID in that letter
+        // make copy of state with the letter updated
+        // return updated state
        return state
    case types.UPDATE_LETTER_WORDID_SUCCESS:
      console.log(state)
