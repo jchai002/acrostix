bc30aa82aa8af45d5612436f4c56e64ef0f33095
Jerry Chai <jerry@gooddogdesign.com>
2016-12-15 02:33:53 -0800

    update word store via reducers

--- client/reducers/wordReducer.js
+++ client/reducers/wordReducer.js
@@ -7,22 +7,17 @@ export default function wordReducer(state = initialState.words,action) {
     case types.ADD_LETTER_TO_WORD_SUCCESS:
       var wordId = action.letter.wordId;
       var stateClone = JSON.parse(JSON.stringify(state));
-      var len = stateClone[wordId].length
-      stateClone[wordId][len] = action.letter
-      // console.log('after push ',word)
+      stateClone[wordId].push(action.letter);
+      console.log(stateClone[wordId])
 
-      console.log('after assign',stateClone[wordId])
-
-      // return [...state,
-      //   Object.assign({}, action.letter)
-      // ]
-      return state
+      return stateClone
     case types.REMOVE_LETTER_FROM_WORD_SUCCESS:
-      console.log('remove from word',action.letter)
-      // return [...state,
-      //   Object.assign({}, action.letter)
-      // ]
-      return state
+      var wordId = action.letter.wordId;
+      console.log(state[wordId])
+      var stateClone = JSON.parse(JSON.stringify(state));
+      stateClone[wordId].pop();
+      console.log(stateClone[wordId])
+      return stateClone
     default:
       return state
   }
