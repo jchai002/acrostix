7d5c26934e8d87d8568d67fc8228dfc405cf41f2
Jerry Chai <jerry@gooddogdesign.com>
2016-12-15 02:22:33 -0800

    word reducer wip

--- client/reducers/initialState.js
+++ client/reducers/initialState.js
@@ -1,4 +1,9 @@
+const Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var words = {};
+for (var i in Alphabet) {
+  words[Alphabet[i]] = [];
+}
 export default {
   letters:[],
-  words:{}
+  words:words
 }
--- client/reducers/wordReducer.js
+++ client/reducers/wordReducer.js
@@ -1,10 +1,18 @@
 import * as types from '../actions/actionTypes.js'
 import initialState from './initialState';
+import update from 'immutability-helper';
 
 export default function wordReducer(state = initialState.words,action) {
   switch (action.type) {
     case types.ADD_LETTER_TO_WORD_SUCCESS:
-      console.log('add to word',action.letter)
+      var wordId = action.letter.wordId;
+      var stateClone = JSON.parse(JSON.stringify(state));
+      var len = stateClone[wordId].length
+      stateClone[wordId][len] = action.letter
+      // console.log('after push ',word)
+
+      console.log('after assign',stateClone[wordId])
+
       // return [...state,
       //   Object.assign({}, action.letter)
       // ]
