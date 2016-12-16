e7261a2ec6e8d19a71e44ca831ecdc0d37a92ce8
Jerry Chai <jerry@gooddogdesign.com>
2016-12-15 17:49:01 -0800

    update app entry point

--- client/main.js
+++ client/main.js
@@ -1,15 +1,18 @@
 import 'babel-polyfill';
 import React from 'react';
+import routes from './routes';
+import { Router, browserHistory } from 'react-router'
 import {render} from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import PuzzleBuilder from './components/puzzleBuilder';
+
 const store = configureStore();
 
 Meteor.startup(() => {
   render(
     <Provider store={store}>
-      <PuzzleBuilder />
+      <Router history={browserHistory} routes= {routes} />
     </Provider>,
     document.getElementById('root')
   );
