507ba8c52c05204f279f2a29e47d012f36a9030d
Jerry Chai <jerry@gooddogdesign.com>
2016-12-24 00:49:53 -0800

    add status to puzzle list

--- client/components/puzzlesList.js
+++ client/components/puzzlesList.js
@@ -11,17 +11,31 @@ class PuzzlesList extends Component {
   renderList() {
     return this.props.puzzles.map(puzzle => {
       const url = `/puzzles/${puzzle._id}`;
-
+      var stage;
+      switch (puzzle.currentStep) {
+        case 1:
+          stage = 'Quote Entry'
+        break;
+        case 2:
+          stage = 'Words Entry'
+        break;
+        case 3:
+          stage = 'Clues Entry'
+        break;
+      }
       return (
         <li className="list-group-item" key={puzzle._id}>
-          <Link to={url}>Puzzle {puzzle._id}</Link>
-          <span>
-            <button
-              className="btn btn-danger"
-              onClick={() => this.onPuzzleRemove(puzzle)}>
-              Remove
-            </button>
-          </span>
+        <span>
+        <span className="tag tag-success">{stage}</span>
+        <Link to={url}>Puzzle {puzzle._id}</Link>
+        </span>
+        <span>
+        <button
+        className="btn btn-danger"
+        onClick={() => this.onPuzzleRemove(puzzle)}>
+        Remove
+        </button>
+        </span>
         </li>
       );
     });
@@ -30,7 +44,7 @@ class PuzzlesList extends Component {
   render() {
     return (
       <ul className="list-group">
-        {this.renderList()}
+      {this.renderList()}
       </ul>
     );
   }
